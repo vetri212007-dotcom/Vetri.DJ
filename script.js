e9ea3b37bcfc926fe2e18d1b2f05ef1016a1cbe2
vetri212007-dotcom <vetri212007@gmail.com>
2025-09-20 16:08:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Select menu toggle and nav links
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+
+// Toggle menu on click
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
